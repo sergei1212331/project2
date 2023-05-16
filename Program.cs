207028c3b2b90c5c9d16271ea3d6e98cb308f47b
sergei1212331 <90478003+sergei1212331@users.noreply.github.com>
2023-05-16 16:12:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int sum = 0;
+
+        Console.WriteLine("Original array: [{0}]", string.Join(", ", arr));
+
+        // Compute the sum of the middle elements
+        for (int i = 1; i < arr.Length - 1; i++)
+        {
+            sum += arr[i];
+        }
+
+        // Divide the sum by 2 and update the array
+        int middleSum = sum / 2;
+        for (int i = 1; i < arr.Length - 1; i++)
+        {
+            arr[i] = middleSum;
+        }
+
+        Console.WriteLine("Array after replacement: [{0}]", string.Join(", ", arr));
+    }
+}
+
+
+
+
+
+
